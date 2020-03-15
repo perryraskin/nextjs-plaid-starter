da8479fff97161bb2cd2040fae04d881908d2002
Perry Raskin <perryraskin@gmail.com>
2020-03-15 00:45:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.prepare().then(() => {
 
   server.listen(port, err => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(`> Ready on ${port}`);
   });
 });
